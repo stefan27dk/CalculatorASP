5122193e1587f196ec1d68f92814179fe20d5e28
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-09-23 13:05:40 +0200

    Add project files.

---
+++ CalculatorASP/Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculatorASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorASP.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new CalculatorModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculatorModel c, string calculate)  // "Calculate is in the button name - name="calculate"
+        {
+            // Add
+            if (calculate == "add")
+            {
+                c.result = c.input1 + c.input2;
+            }
+
+            // Minus
+           else if (calculate == "min")
+            {
+                c.result = c.input1 - c.input2;
+            }
+
+
+            // Subtract
+            else if (calculate == "sub")
+            {
+                c.result = c.input1 * c.input2;
+            }
+
+
+            // Divide
+            else
+            {
+                c.result = c.input1 / c.input2;
+            }
+
+            return View(c);
+        }
+
+    }
+}
---
+++ CalculatorASP/Models/CalculatorModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorASP.Models
+{
+    public class CalculatorModel
+    {
+        public int input1 { get; set; }
+        public int input2 { get; set; }
+        public int result { get; set; }
+    }
+}
